30826bf38a7a22e4c6f7f455666d3412af8dbb51
tom <i@aolose.cc>
2021-02-26 23:48:19 +0800

    ~

--- rootTmplCsv.js
+++ rootTmplCsv.js
@@ -1,21 +1,32 @@
 const data1 = require('./merged0.json')
 const data2 = require('./merged2.json')
-let str = `"Name","Type","Stats","ID","EquipmentSlots"\n`;
+
+
+const fs = require('fs')
+const m={}
+const nm=fs.readFileSync(__dirname+"/english.xml").toString()
+nm.split(/\r?\n+/).forEach(v=>{
+    if(v){
+        const [,k,vv] = /contentuid="(\w+)" version="1">(.+?)</.exec(v) || [];
+        if(k&&vv){
+            m[k]=vv
+        }
+    }
+})
+
+let str = `"ID","Name","Type","Stat","Desc"\n`;
 [data1, data2].forEach(data => {
     const {save: {regions: {Templates: {GameObjects: items}}}} = data;
     items.forEach((i) => {
-        const {
-            Name: {value: v0 = ""} = {},
-            Type: {value: v1 = ""} = {},
-            Stats: {value: v2 = ""} = {}
-            , MapKey: {value: v3 = ""} = {}
+        let {
+            MapKey: {value: v0 = ""} = {},
+            DisplayName: {handle: v1 = ""} = {},
+            Type: {value: v2 = ""} = {},
+            Stats: {value: v3 = ""} = {},
+            Description: {handle: v4 = ""} = {}
         } = i
-        v4="";
-        if(Array.isArray(i.Equipment)){
-           try {
-               v4=i.Equipment[0].EquipmentSlots.value
-           }catch (e){}
-        }
+        if(v1)v1=m[v1]||v1;
+        if(v4)v4=m[v4]||v4;
         str += `"${v0}","${v1}","${v2}","${v3}","${v4}"\n`
     })
 })
