f207992913f3bd75bb053fb5cbb85fc295ee6e28
Tom Z <anytable@gmail.com>
2024-09-22 00:09:47 +0800

    optimize sw

--- src/registerSW.js
+++ src/registerSW.js
@@ -1,64 +1,36 @@
-if ('serviceWorker' in navigator) {
-  let register;
-  let worker;
-  let promptTimer = -1;
-
-  const prompt = document.getElementById('k');
-  prompt.onclick = ({ target }) => {
-    if ('B' === target.tagName[0]) {
-      prompt.classList.remove('a');
-      if (target.textContent[0] === 'R') {
-        setTimeout(() => {
+const { serviceWorker } = navigator;
+!(async function () {
+  if (serviceWorker) {
+    const prompt = document.getElementById('k');
+    prompt.onclick = ({ target }) => {
+      if ('B' === target.tagName[0]) {
+        prompt.classList.remove('a');
+        if (target.textContent[0] === 'R') {
           location.reload();
-        }, 200);
+        }
       }
-    }
-  };
+    };
+    let register = await serviceWorker.getRegistration();
+    if (!register) {
+      const sw =
+        import.meta.env.MODE === 'production' ? '/sw.js' : '/dev-sw.js?dev-sw';
+      register = await serviceWorker.register(sw, { scope: '/' });
+      let updated;
+      register.onupdatefound = (e) => {
+        if (updated) prompt.classList.add('a');
+        updated = 1;
+      };
+    } else
+      setInterval(() => {
+        const worker = register.waiting;
+        if (worker?.state === 'installed') {
+          worker.postMessage({ type: 'SKIP_WAITING' });
+          prompt.classList.add('a');
+        }
+      }, 5e3);
 
-  const untilWaitingInstalled = () => {
-    if (worker.state !== 'installed') return true;
-    clearInterval(promptTimer);
-    worker.postMessage({ type: 'SKIP_WAITING' });
-    prompt.classList.add('a');
-  };
-
-  const untilWaitingFound = () => {
-    if (register.waiting) {
-      worker = register.waiting;
-      if (untilWaitingInstalled()) {
-        worker.removeEventListener('statechange', untilWaitingInstalled);
-        worker.addEventListener('statechange', untilWaitingInstalled);
-      }
-    } else return true;
-  };
-
-  const checkUpdate = () => {
-    promptTimer = setTimeout(() => {
-      register
-        .update()
-        .then(() => handle())
-        .catch(checkUpdate);
-    }, 5e3); // 5min loop
-  };
-
-  const handle = (r) => {
-    if (r) register = r;
-    if (!register) return;
-    checkUpdate();
-    if (untilWaitingFound()) {
-      register.removeEventListener('updatefound', untilWaitingFound);
-      register.addEventListener('updatefound', untilWaitingFound);
-    }
-  };
-
-  navigator.serviceWorker
-    .register(
-      import.meta.env.MODE === 'production' ? '/sw.js' : '/dev-sw.js?dev-sw',
-      { scope: '/' }
-    )
-    .then(handle)
-    .catch(() => {
-      clearTimeout(promptTimer);
-      register?.waiting?.postMessage?.({ type: 'SKIP_WAITING' });
-    });
-}
+    setInterval(() => {
+      if (!register.waiting) register.update().catch(console.error);
+    }, 12e4); // 2min
+  }
+})();
